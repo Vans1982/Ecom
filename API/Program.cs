36b035d479b98da20f6de1bc0c180cc1f57c71d7
Vans <36137548+Vans1982@users.noreply.github.com>
2023-02-14 22:11:29 +0200

    New Interface changes made

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
