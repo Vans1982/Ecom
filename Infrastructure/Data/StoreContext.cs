2da047da6140260db0a8339b259b339d58f5ee8d
Niven Pillay <nivendran.pillay@gmail.com>
2023-03-08 13:10:29 +0200

    Creating and Configuring Migrations

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -11,6 +12,12 @@ namespace Infrastructure.Data
         public DbSet<Product> Products{get;set;}
         public DbSet<ProductBrand> ProductBrands{get;set;}
         public DbSet<ProductType> ProductsTypes{get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           base.OnModelCreating(modelBuilder);
+           modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
 
